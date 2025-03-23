2cd498a357e85f3fc7b5671cfcba4650cdfb493e
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-03-22 22:57:45 -0500

    fix: Se corrigen algunos errores al elegir las opciones en el menu de pedidos

--- Menu_pedidos.py
+++ Menu_pedidos.py
@@ -7,19 +7,22 @@ def menu_pedidos():
                     1. Creacion de pedidos
                     2. Registro de productos
                     3. Editar pedidos
+                    4. Eliminar pedidos
+                    5. Retroceder
                     """)
                 print("Ingrese una opcion")
                 decision=int(input("Opcion:"))
                 if decision>4:
                     print("No existe esa opcion (opcion no valida)")
                 if decision==1:
-                    print("Creacion de productos")
+                    print("Creacion de pedidos")
                 elif decision==2:
-                    print("Registro de productos")
+                    print("Registro de pedidos")
                 elif decision==3:
-                    print("Editar productos")
+                    print("Editar pedidos")
                 elif decision==4:
-                    print("Retrocediendo")
+                    print("Eliminar pedidos")
+                elif decision==5:
                     inicio_principal()
         except ValueError:
             print("Error, ingreso de valor no valido en el programa, Devuelto al menu")
